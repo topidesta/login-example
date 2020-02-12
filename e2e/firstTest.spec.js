dcb77ebfbc7a506575446602a8245596bc52dd09
cgarcete-technisys <cgarcete@technisys.com>
2020-02-12 12:14:20 -0300

    sacar el firstTest

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,16 +1,16 @@
-import * as detox from 'detox';
+// import * as detox from 'detox';
 
-describe('Several tests on screen', () => {
-  beforeEach(async () => {
-    await detox.device.reloadReactNative();
-  });
+// describe('Several tests on screen', () => {
+//   beforeEach(async () => {
+//     await detox.device.reloadReactNative();
+//   });
 
-  it('should show Ingresar datos textField:', async () => {
-    await expect(detox.element(detox.by.text('Ingresar datos:'))).toBeVisible();
-  });
+//   it('should show Ingresar datos textField:', async () => {
+//     await expect(detox.element(detox.by.text('Ingresar datos:'))).toBeVisible();
+//   });
 
-  it('should complete Nombre and Apellido input', async () => {
-    await detox.element(detox.by.id('name')).typeText('Claudio');
-    await detox.element(detox.by.id('lastName')).typeText('Garcete');
-  });
-});
+//   it('should complete Nombre and Apellido input', async () => {
+//     await detox.element(detox.by.id('name')).typeText('Claudio');
+//     await detox.element(detox.by.id('lastName')).typeText('Garcete');
+//   });
+// });
